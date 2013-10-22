7ab041f47b8a25bf4c4de10e49038e413d3608bd
Firas Bessadok <firas.bess@gmail.com>
2013-10-23 00:36:53 +0200

    almost...

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,50 +2,78 @@ package controllers
 
 import scala.concurrent._
 import ExecutionContext.Implicits.global
- 
+
 import play.api.libs.iteratee._
- 
+
 import play.api._
 import play.api.mvc._
 import play.api.libs._
 import play.api.libs.ws._
- 
+
 import play.api.libs.json._
 import play.api.libs.functional._
 import play.api.libs.functional.syntax._
 import play.api.libs.json.Reads._
 
 object Application extends Controller {
-  
+
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
   }
   
+  implicit val writeVenueAsJson = Json.writes[Venue]
+
   def trending(lat: Double, lng: Double, oauth_token: String) = Action {
+    val asJson: Enumeratee[Venue,JsValue] = Enumeratee.map { 
+      case t => Json.toJson(t)
+    }
     Async {
-      Venue.fetchPage(lat, lng, oauth_token).map { case json => Ok(json) } 
+      Venue.fetchPage(lat, lng, oauth_token).map { case json => Ok(json &> asJson &> EvenSource()) } 
     }
   }
 
-  case class Venue(id: String, name: String, lat: Double, lng: Double)
-  
+  case class Venue(id: String, name: String, lat: Double, lng: Double, hereNow: Int)
+
   object Venue {
-    def search(lat: Double, lng: Double): Enumerator[Venue] = {
+    def search(lat: Double, lng: Double, oauth_token: String): Enumerator[Venue] = {
+      
       ???
+      
     }
-    
-    def fetchPage(lat: Double, lng: Double, oauth_token: String): Future[String] = {
+
+    implicit def convertVenue: Reads[Seq[Venue]] =
+      (__ \ "response").read(
+        seq(
+          (__ \ "id").read[String] and
+          (__ \ "name").read[String] and
+          (__ \ "location").read(
+            (__ \ "lat").read[Double] and
+            (__ \ "lng").read[Double] tupled
+          ) and
+          (__ \ "hereNow").read(
+            (__ \ "count").read[Int]
+          )
+          tupled
+        )
+      ).map(
+        _.collect {
+          case (id, name, (lat, lng), hereNow) => Venue(id, name, lat, lng, hereNow)
+        }
+      )
+
+    def fetchPage(lat: Double, lng: Double, oauth_token: String): Future[Seq[Venue]] = {
       WS.url("https://api.foursquare.com/v2/venues/trending").withQueryString(
         "ll" -> (lat.toString + "," + lng.toString),
+        "limit" -> "50",
+        "radius" -> "2000",
         "oauth_token" -> oauth_token,
         "v" -> "20131022"
       ).get().map(
         r => r.status match {
-          case 200 => r.body
-          case e => "Bad response"
+          case 200 => r.json.asOpt[Seq[Venue]].getOrElse(Nil)
+          case e => sys.error(s"Bad response. Status $e")
         }
       )
     }
   }
-  
 }
